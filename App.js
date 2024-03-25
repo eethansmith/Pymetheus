09060cedea992e835691da46964813581a00cb77
psyes9 <ethan.a.smith@hotmail.co.uk>
2024-03-25 15:02:13 +0000

    Main Menu Added navigation installed

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Customizable button */}
+      <TouchableOpacity style={styles.button} onPress={() => alert("Button Pressed")}>
+        <Text style={styles.buttonText}>PYMETHEUS</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#202124',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: 'transparent', // Transparent background
+    padding: 10,
+    borderRadius: 5,
+    borderWidth: 2, // Border width for the outline
+    borderColor: '#bdc1c6', // Border color matching the background
+  },
+  buttonText: {
+    color: '#ea80fc', // Button text color
+    fontSize: 18, // Font size
+    fontFamily: 'Avenir', // Font family (make sure the font is available)
   },
 });
